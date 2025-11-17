13b6c053d11f897eca337bace368843bb4d8bd07
domespa <dumiii1988@gmail.com>
2025-11-17 09:43:07 +0100

    gdf

--- frontend/src/components/editor/CreatePost.tsx
+++ frontend/src/components/editor/CreatePost.tsx
@@ -484,8 +484,6 @@ export const CreatePost = () => {
           </div>
         </details>
 
-        {/* TODO: Featured Image & Gallery (Milestone successiva) */}
-
         {/* Errore generale */}
         {errors.general && (
           <div className="bg-red-50 border border-red-200 text-red-800 px-4 py-3 rounded-lg">
